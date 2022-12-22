c26bc2edb853eaa3906e44d06e373737cc43cee1
pltduchoang <97260149+pltduchoang@users.noreply.github.com>
2022-12-22 14:05:45 -0700

    first

---
+++ javascript/javascript.js
@@ -0,0 +1,54 @@
+let start = document.getElementById("start")
+let skip = document.getElementById("skip")
+let music = document.getElementById("backgroundmusic")
+let cover = document.getElementById("cover")
+let musicbutton = document.getElementById("musicbutton")
+let content = document.getElementById("content")
+
+
+start.addEventListener("click",startmusic)
+skip.addEventListener("click",stopmusic)
+
+function startmusic () {
+  music.play();
+  cover.classList.add("hide")
+  musicbutton.innerHTML = "Pause Background Music"
+  content.classList.remove("hide")
+}
+
+function stopmusic () {
+  music.pause()
+  cover.classList.add("hide")
+  musicbutton.innerHTML = "Play Background Music"
+  content.classList.remove("hide")
+}
+
+musicbutton.addEventListener("click",playPauseMusic)
+
+function playPauseMusic () {
+  if (music.paused) {
+    music.play();
+    musicbutton.innerHTML = "Pause Background Music"
+  } else {
+    music.pause();
+    musicbutton.innerHTML = "Play Background Music"
+  }
+}
+
+let slideIndex = 0;
+showSlides();
+
+let iframe = document.getElementsByTagName("iframe")
+iframe.addEventListener("click",playPauseMusic)
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
